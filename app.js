c41ac31f696d6418264c4512b0d4ec97fc35d4d4
ing-arriola <ingeniero.arriola@gmail.com>
2020-02-22 19:48:51 -0600

    Added second promise

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+//This is an example of promises
+/*
+The sintaxis is:
+ES6:
+someName=new Promise((resolve,reject)=>{
+    An or many action(s) to be executed, also called executor
+})
+.someName.then((argsFrom actions executed)=>{
+    More actions
+})
+.
+
+*/
+//First example
 exampleOfPromise=new Promise((resolve,reject)=>{
     setTimeout(()=>{
         resolve("Funciona")
@@ -5,5 +19,23 @@ exampleOfPromise=new Promise((resolve,reject)=>{
 })
 
 exampleOfPromise.then((mensaje)=>{
+    console.log(mensaje)
+})
+
+//Second example
+secondPromise=new Promise((resolver,rechazar)=>{
+    let estado=false
+
+    if(estado){
+        resolver("todo bien, todo correcto")
+    }else{
+        rechazar("Triste....")
+    }
+})
+
+secondPromise.then((mensajito)=>{
+    console.log(mensajito)
+})
+.catch((mensaje)=>{
     console.log(mensaje)
 })
